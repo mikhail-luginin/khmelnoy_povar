05dabbf60d7070856a148c13a86e5784bf5bfb24
mikhail-luginin <mlugnn@gmail.com>
2023-05-06 19:00:44 +0300

    Updated: urls.py for static files with debug=false

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.static import static
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import redirect
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('', lambda x: redirect(settings.LOGIN_REDIRECT_URL)),
@@ -12,9 +13,9 @@ urlpatterns = [
     path('bar/', include('apps.bar.urls')),
     path('repairer/', include('apps.repairer.urls')),
     path('api/', include('apps.api.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^assets/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout')
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
