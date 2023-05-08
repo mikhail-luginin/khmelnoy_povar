3ec16c872d99b71ea912ccd44d37c3271860eabf
mikhail-luginin <125300555+mikhail-luginin@users.noreply.github.com>
2023-05-08 21:47:34 +0300

    Updated: remake timetable row creating

--- app/apps/lk/services/timetable.py
+++ app/apps/lk/services/timetable.py
@@ -1,4 +1,9 @@
+from django.db.models import Sum
+
 from apps.bar.models import Timetable
+from apps.lk.models import Fine
+
+from global_services.salary import SalaryService
 
 from typing import List
 
@@ -8,3 +13,23 @@ class TimetableService:
 
     def timetable_all(self) -> List[model]:
         return self.model.objects.all()
+
+    def create(self, date_at: str, employee_id: int, storage_id: int, position_id: int, oklad: int) -> bool:
+        timetable = self.model()
+        timetable.date_at = date_at
+        timetable.employee_id = employee_id
+        timetable.storage_id = storage_id
+        timetable.position_id = position_id
+        timetable.oklad = oklad
+        timetable.save()
+
+        money_data = SalaryService().calculate_prepayment_salary_by_timetable_object(timetable_object=timetable)
+
+        timetable.percent = money_data['percent']
+        timetable.premium = money_data['premium']
+
+        fine = Fine.objects.filter(employee_id=employee_id, date_at=date_at).aggregate(Sum("sum"))['sum__sum']
+        timetable.fine = int(fine)
+        timetable.save()
+
+        return True
