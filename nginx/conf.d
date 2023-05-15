7698f6d8af3882d0e4934937e908c70a3f0a5373
mikhail-luginin <125300555+mikhail-luginin@users.noreply.github.com>
2023-05-15 18:31:41 +0300

    Fixed: 1st try to fix ports and static files

--- nginx/conf.d
+++ nginx/conf.d
@@ -1,17 +1,21 @@
 upstream app {
-    server app:80;
+    server app:8000;
 }
 
 server {
 
     listen 80;
     server_name ${FIRST_ADDRESS} ${SECOND_ADDRESS}
+
     location / {
         proxy_pass http://app;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-Proto $scheme;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header Host $host;
+        proxy_redirect off;
+    }
+
+    location /assets/ {
+        alias app/assets/;
     }
 
 }
