77bced819eed89be5b63af06d497038fb31008c5
mikhail-luginin <125300555+mikhail-luginin@users.noreply.github.com>
2023-05-22 20:06:52 +0300

    Added: get object param for get work employee

--- app/apps/lk/services/timetable.py
+++ app/apps/lk/services/timetable.py
@@ -66,5 +66,7 @@ class TimetableService:
         else:
             raise self.model.DoesNotExist('Запись с указанным идентификатором не найдена.')
 
-    def is_employee_work_on_date(self, employee_id: int, date_at: str) -> bool:
-        return self.model.objects.filter(employee_id=employee_id, date_at=date_at).exists()
+    def is_employee_work_on_date(self, employee_id: int, date_at: str, get_object: bool = False) -> bool:
+        qs = self.model.objects.filter(employee_id=employee_id, date_at=date_at)
+
+        return qs.first() if get_object else qs.exists()
