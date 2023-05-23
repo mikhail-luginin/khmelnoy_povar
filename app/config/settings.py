59c4c38014b436222b8215fe26501b923d2a5e00
mikhail-luginin <125300555+mikhail-luginin@users.noreply.github.com>
2023-05-23 19:40:40 +0300

    Updated: date format for drf

--- app/config/settings.py
+++ app/config/settings.py
@@ -44,7 +44,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ]
+    ],
+    'DATETIME_FORMAT': "%Y-%m-%d %H:%M:%S"
 }
 
 MIDDLEWARE = [
