7370ad4df1d9d96f4c23fa6db77b32453d9aebd7
mikhail-luginin <125300555+mikhail-luginin@users.noreply.github.com>
2023-05-23 15:48:08 +0300

    Fixed: ajax_columns

--- app/assets/js/datatables_custom_funcs.js
+++ app/assets/js/datatables_custom_funcs.js
@@ -1,33 +1,6 @@
 function run_datatable(table_id, ajax_url, url, ajax_columns,
                        has_delete = true, has_update = false,
                        has_dismiss = false, has_edit = true, has_actions = true, custom_dict = {}) {
-    if (has_actions == true) {
-        ajax_columns.concat([
-            {
-                "data": null,
-                render: function (data, type, row) {
-                    let actions = ''
-                    if (has_edit == true) {
-                        actions += '<a href="' + url + '/edit?id=' + row.id + '"><i class="fa fa-edit"></i></a>'
-                    }
-                    if (has_delete == true) {
-                        actions += '<a href="#" onclick="delete_confirm(' + '\'' + row.id + '\'' + ', ' + '\'Вы действительно хотите удалить данную запись?\'' + ', ' + '\'' + url + '/delete' + '\'' + ')"><i class="fa fa-trash"></i></a>'
-                    }
-                    if (has_update == true) {
-                        actions += '<a href="' + url + '/update?id=' + row.id + '"><i class="fa fa-cloud-upload"></i></a>'
-                    }
-                    if (has_dismiss == true) {
-                        if (row.active_status === 'Активный') {
-                            actions += '<a href="#" onclick="delete_confirm(' + '\'' + row.id + '\'' + ', ' + '\'Вы действительно хотите уволить ' + row.fio + '?\'' + ', ' + '\'' + url + '/dismiss' + '\'' + ', ' + '\'' + 'Уволить' + '\'' + ')"><i class="fa fa-user-minus"></i></a>'
-                        } else {
-                            actions += '<a href="' + url + '/return?id=' + row.id + '"<i class="fa fa-user"></i</a>'
-                        }
-                    }
-                    return actions
-                }
-            }
-        ])
-    }
     let datatable_dict = {
         language: {
             "url": "//cdn.datatables.net/plug-ins/1.11.1/i18n/ru.json"
@@ -59,6 +32,33 @@ function run_datatable(table_id, ajax_url, url, ajax_columns,
         }
 
 
+    }
+    if (has_actions === true) {
+        datatable_dict['columns'] = ajax_columns.concat([
+            {
+                "data": null,
+                render: function (data, type, row) {
+                    let actions = ''
+                    if (has_edit === true) {
+                        actions += '<a href="' + url + '/edit?id=' + row.id + '"><i class="fa fa-edit"></i></a>'
+                    }
+                    if (has_delete === true) {
+                        actions += '<a href="#" onclick="delete_confirm(' + '\'' + row.id + '\'' + ', ' + '\'Вы действительно хотите удалить данную запись?\'' + ', ' + '\'' + url + '/delete' + '\'' + ')"><i class="fa fa-trash"></i></a>'
+                    }
+                    if (has_update === true) {
+                        actions += '<a href="' + url + '/update?id=' + row.id + '"><i class="fa fa-cloud-upload"></i></a>'
+                    }
+                    if (has_dismiss === true) {
+                        if (row.active_status === 'Активный') {
+                            actions += '<a href="#" onclick="delete_confirm(' + '\'' + row.id + '\'' + ', ' + '\'Вы действительно хотите уволить ' + row.fio + '?\'' + ', ' + '\'' + url + '/dismiss' + '\'' + ', ' + '\'' + 'Уволить' + '\'' + ')"><i class="fa fa-user-minus"></i></a>'
+                        } else {
+                            actions += '<a href="' + url + '/return?id=' + row.id + '"<i class="fa fa-user"></i</a>'
+                        }
+                    }
+                    return actions
+                }
+            }
+        ])
     }
     let table = $('#' + table_id).DataTable(Object.assign(datatable_dict, custom_dict))
     // Apply column filter
