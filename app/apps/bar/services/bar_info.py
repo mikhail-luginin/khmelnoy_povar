d205f84f20a0446d115cdf36abc957a68844ddad
glebsolovyov <solovyovgleb1@gmail.com>
2023-06-07 02:30:50 +0300

    Update: new logic

--- app/apps/bar/services/bar_info.py
+++ app/apps/bar/services/bar_info.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 from typing import Type
 
@@ -7,7 +8,7 @@ from apps.lk.models import Position, JobPlace, Employee
 from apps.iiko.models import Storage, PaymentType
 
 from core import total_values
-from core.time import today_date
+from core.time import today_date, get_current_time
 
 
 def get_bar(**kwargs) -> Storage | None:
@@ -42,6 +43,12 @@ def get_main_barmen(date_at: str, storage: Storage) -> Employee | None:
     else:
         return None
 
+def get_full_information_of_day_for_data_logs(days: str, storage: Storage):
+    if not days:
+        days = 0
+    date = (get_current_time() + datetime.timedelta(days=int(days))).strftime('%Y-%m-%d')
+    return get_full_information_of_day(date_at=date, storage=storage)
+
 
 def get_full_information_of_day(date_at: str, storage: Storage) -> dict:
     try:
@@ -49,18 +56,26 @@ def get_full_information_of_day(date_at: str, storage: Storage) -> dict:
     except Money.DoesNotExist:
         return dict(error=True)
 
-    total_day = 0
-    cash = 0
-    cash_point = 0
-    total_cashshifts = 0
-    delivery = 0
-    yandex = 0
+    today = None
 
-    open_date = None
-    close_date = None
-    session_number = None
+    sum_cash_morning = money_row.sum_cash_morning
+    sum_cash_end_day = money_row.sum_cash_end_day
+    calculated = money_row.calculated
+    difference = money_row.difference
 
     if date_at == today_date():
+
+        total_day = 0
+        cash = 0
+        cash_point = 0
+        total_cashshifts = 0
+        delivery = 0
+        yandex = 0
+
+        open_date = None
+        close_date = None
+        session_number = None
+
         json_data = json.loads(IikoService().get_cashshifts(date_at, date_at))
         for row in json_data:
             if row["pointOfSaleId"] == storage.point_of_sale:
@@ -96,24 +111,39 @@ def get_full_information_of_day(date_at: str, storage: Storage) -> dict:
         total_market = delivery + yandex
         total_cash = total_day - total_cashshifts + cash + cash_point
         total_bn = total_day - total_cash - total_market
+
+        if not total_day:
+            total_day = 0
+        if not total_market:
+            total_market = 0
+
+        sum_cash_morning = money_row.sum_cash_morning
+        sum_cash_end_day = money_row.sum_cash_end_day
+        calculated = money_row.calculated
+        difference = money_row.difference
+        today = True
+
     else:
+        session_number = money_row.session.session_number
+        open_date = money_row.session.open_date
+        close_date = money_row.session.close_date
         total_day = money_row.total_day
-        total_bn = money_row.total_bn
         total_market = money_row.total_market
         total_cash = money_row.total_cash
+        total_bn = money_row.total_bn
+        cash = money_row.session.cash
+        cash_point = money_row.session.cash_point
+        yandex = money_row.session.yandex
+        delivery = money_row.session.delivery
 
-    if not total_day:
-        total_day = 0
-    if not total_market:
-        total_market = 0
-
-    return dict(session_number=session_number, open_date=open_date, close_date=close_date,
+    return dict(storage=storage, date_at=date_at, today=today, session_number=session_number, open_date=open_date,
+                close_date=close_date,
                 total_day=total_day, sum_for_percent=total_day - total_market,
                 total_cash=total_cash, total_bn=total_bn,
                 cash=cash, cash_point=cash_point,
                 yandex=yandex, delivery=delivery,
-                sum_cash_morning=money_row.sum_cash_morning, sum_cash_end_day=money_row.sum_cash_end_day,
-                calculated=money_row.calculated, difference=money_row.difference,
+                sum_cash_morning=sum_cash_morning, sum_cash_end_day=sum_cash_end_day,
+                calculated=calculated, difference=difference,
                 expenses_nal=total_values.get_total_expenses_by_date_and_storage(storage, date_at, False),
                 salary_prepayment=total_values.get_total_salary_by_date_and_storage(storage, date_at, 1),
                 salary_month=total_values.get_total_salary_by_date_and_storage(storage, date_at, 2),
