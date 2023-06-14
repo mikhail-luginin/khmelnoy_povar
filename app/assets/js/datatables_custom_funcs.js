c90cdc915877b690f6b33b919de890a28fd390c8
mikhail-luginin <125300555+mikhail-luginin@users.noreply.github.com>
2023-06-14 14:47:25 +0300

    Add run datatable plugin with json function

--- app/assets/js/datatables_custom_funcs.js
+++ app/assets/js/datatables_custom_funcs.js
@@ -89,3 +89,61 @@ function run_datatable(table_id, ajax_url, url, ajax_columns,
     })
     return table
 }
+
+function run_datatable_with_json(table_id, json, columns) {
+    let datatable_dict = {
+        language: {
+            "url": "//cdn.datatables.net/plug-ins/1.11.1/i18n/ru.json"
+        },
+        lengthMenu: [10, 25, 50, 100, 200, 500],
+        pageLength: 25,
+        responsive: true,
+
+        data: json,
+        columns: columns,
+        footerCallback: function (row, data, start, end, display) {
+            var api = this.api();
+            var total = [];
+            for (var i = 0; i < api.columns().count(); i++) {
+                pageTotal = api.column(i, {page: 'current'}).data().reduce(function (a, b) {
+                    return (parseFloat(a) || 0) + (parseFloat(b) || 0);
+                }, 0);
+                total[i] = pageTotal;
+            }
+            // Update footer with totals for relevant columns
+            $(api.columns().footer()).html(function (index) {
+                return total[index];
+            });
+        }
+
+
+    }
+    let table = $('#' + table_id).DataTable(datatable_dict)
+    // Apply column filter
+    $('#' + table_id + ' .dt-column-filter th').each(function (i) {
+        $('input', this).on('keyup change', function () {
+            if (table.column(i).search() !== this.value) {
+                table
+                    .column(i)
+                    .search(this.value)
+                    .draw()
+            }
+        })
+    })
+    // Toggle Column filter function
+    var responsiveFilter = function (table, index, val) {
+        var th = $(table).find('.dt-column-filter th').eq(index)
+        val === true ? th.removeClass('d-none') : th.addClass('d-none')
+    }
+    // Run Toggle Column filter at first
+    $.each(table.columns().responsiveHidden(), function (index, val) {
+        responsiveFilter('#' + table_id, index, val)
+    })
+    // Run Toggle Column filter on responsive-resize event
+    table.on('responsive-resize', function (e, datatable, columns) {
+        $.each(columns, function (index, val) {
+            responsiveFilter('#' + table_id, index, val)
+        })
+    })
+    return table
+}
