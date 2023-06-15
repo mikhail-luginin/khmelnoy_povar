b451de3cc4c52cf796da8a291bb6b57495205fb1
mikhail-luginin <125300555+mikhail-luginin@users.noreply.github.com>
2023-06-15 15:24:25 +0300

    Media path

--- app/config/settings.py
+++ app/config/settings.py
@@ -136,7 +136,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
