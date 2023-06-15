6a27ffeefea49fd9579c3979fe8592fdedf2e105
mikhail-luginin <125300555+mikhail-luginin@users.noreply.github.com>
2023-06-15 03:12:45 +0300

    Fix ports

--- nginx/conf.d
+++ nginx/conf.d
@@ -8,7 +8,7 @@ server {
     server_name ${FIRST_ADDRESS} ${SECOND_ADDRESS}
 
     location / {
-        proxy_pass http://app;
+        proxy_pass http://app:8080;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header Host $host;
         proxy_redirect off;
