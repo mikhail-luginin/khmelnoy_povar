f413db7c523524d82fe635692c213ab2077c8325
mikhail-luginin <125300555+mikhail-luginin@users.noreply.github.com>
2023-06-19 13:40:48 +0300

    Fix update

--- app/apps/lk/services/money.py
+++ app/apps/lk/services/money.py
@@ -23,93 +23,92 @@ class MoneyService:
         if type(row_id) is str:
             validators.validate_field(row_id, 'идентификатор записи')
 
-        money_record = self.model.objects.filter(id=row_id)
-        if money_record.exists():
-            money_record = money_record.first()
+        money_record = self.model.objects.filter(id=row_id).first()
+        if money_record:
+            if money_record.sum_cash_end_day and money_record.sum_cash_end_day != 0:
+                date_at = money_record.date_at
+                storage = money_record.storage
 
-            date_at = money_record.date_at
-            storage = money_record.storage
+                expenses = get_total_expenses_by_date_and_storage(storage, date_at, False)
+                salary_avans = get_total_salary_by_date_and_storage(storage, date_at, 1)
+                salary_calculated = get_total_salary_by_date_and_storage(storage, date_at, 2)
+                payin = get_total_payin_by_date_and_storage(storage, date_at)
+                payout = get_total_payout_by_date_and_storage(storage, date_at)
+                point_of_sale = storage.point_of_sale
 
-            expenses = get_total_expenses_by_date_and_storage(storage, date_at, False)
-            salary_avans = get_total_salary_by_date_and_storage(storage, date_at, 1)
-            salary_calculated = get_total_salary_by_date_and_storage(storage, date_at, 2)
-            payin = get_total_payin_by_date_and_storage(storage, date_at)
-            payout = get_total_payout_by_date_and_storage(storage, date_at)
-            point_of_sale = storage.point_of_sale
+                total_day = 0
+                cash = 0
+                cash_point = 0
+                delivery = 0
+                yandex = 0
+                total_cashshifts = 0
 
-            total_day = 0
-            cash = 0
-            cash_point = 0
-            delivery = 0
-            yandex = 0
-            total_cashshifts = 0
+                open_date = None
+                close_date = None
+                session_number = None
 
-            open_date = None
-            close_date = None
-            session_number = None
+                cashshifts_data = json.loads(IikoService().get_cashshifts(str(date_at), str(date_at)))
 
-            cashshifts_data = json.loads(IikoService().get_cashshifts(str(date_at), str(date_at)))
+                for i in range(len(cashshifts_data)):
+                    if cashshifts_data[i]["pointOfSaleId"] == point_of_sale:
+                        first_data = IikoService().get_sales_by_department(cashshifts_data[i]["id"])
+                        data = json.loads(first_data)
+                        total_day += cashshifts_data[i]["payOrders"]
 
-            for i in range(len(cashshifts_data)):
-                if cashshifts_data[i]["pointOfSaleId"] == point_of_sale:
-                    first_data = IikoService().get_sales_by_department(cashshifts_data[i]["id"])
-                    data = json.loads(first_data)
-                    total_day += cashshifts_data[i]["payOrders"]
+                        open_date = cashshifts_data[i]["openDate"]
+                        if open_date is not None:
+                            open_date = f'{open_date.split("T")[0]} {open_date.split("T")[1]}'
+                        close_date = cashshifts_data[i]["closeDate"]
+                        if close_date is not None:
+                            close_date = f'{close_date.split("T")[0]} {close_date.split("T")[1]}'
 
-                    open_date = cashshifts_data[i]["openDate"]
-                    if open_date is not None:
-                        open_date = f'{open_date.split("T")[0]} {open_date.split("T")[1]}'
-                    close_date = cashshifts_data[i]["closeDate"]
-                    if close_date is not None:
-                        close_date = f'{close_date.split("T")[0]} {close_date.split("T")[1]}'
+                        session_number = cashshifts_data[i]["sessionNumber"]
 
-                    session_number = cashshifts_data[i]["sessionNumber"]
+                        for a in range(len(data["cashlessRecords"])):
+                            row = PaymentType.objects.get(payment_id=data["cashlessRecords"][a]["info"]["paymentTypeId"])
+                            if row.name == 'Наличные':
+                                cash += int(data["cashlessRecords"][a]["info"]["sum"])
+                            if row.name == 'Наличные.':
+                                cash_point += int(data["cashlessRecords"][a]["info"]["sum"])
+                            if row.name == 'Delivery Club':
+                                delivery += int(data["cashlessRecords"][a]["info"]["sum"])
+                            if row.name == 'Яндекс ЕДА' or row.name == '-Яндекс ЕДА':
+                                yandex += int(data["cashlessRecords"][a]["info"]["sum"])
+                            total_cashshifts += data["cashlessRecords"][a]["info"]["sum"]
+                nal = int(total_day) - int(total_cashshifts)
+                sum_nal = int(cash) + nal + int(cash_point)
+                sum_bn = int(total_day) - sum_nal - int(yandex) - int(delivery)
 
-                    for a in range(len(data["cashlessRecords"])):
-                        row = PaymentType.objects.get(payment_id=data["cashlessRecords"][a]["info"]["paymentTypeId"])
-                        if row.name == 'Наличные':
-                            cash += int(data["cashlessRecords"][a]["info"]["sum"])
-                        if row.name == 'Наличные.':
-                            cash_point += int(data["cashlessRecords"][a]["info"]["sum"])
-                        if row.name == 'Delivery Club':
-                            delivery += int(data["cashlessRecords"][a]["info"]["sum"])
-                        if row.name == 'Яндекс ЕДА' or row.name == '-Яндекс ЕДА':
-                            yandex += int(data["cashlessRecords"][a]["info"]["sum"])
-                        total_cashshifts += data["cashlessRecords"][a]["info"]["sum"]
-            nal = int(total_day) - int(total_cashshifts)
-            sum_nal = int(cash) + nal + int(cash_point)
-            sum_bn = int(total_day) - sum_nal - int(yandex) - int(delivery)
+                calculated = money_record.sum_cash_morning + sum_nal - expenses - salary_calculated - salary_avans - payout + payin
 
-            calculated = money_record.sum_cash_morning + sum_nal - expenses - salary_calculated - salary_avans - payout + payin
+                money_record.total_day = total_day
+                money_record.total_cash = sum_nal
+                money_record.total_bn = sum_bn
+                money_record.total_market = yandex + delivery
+                money_record.total_expenses = expenses
+                money_record.total_salary = salary_avans + salary_calculated
+                money_record.total_payin = payin
+                money_record.total_payout = payout
+                money_record.calculated = calculated
+                money_record.difference = money_record.sum_cash_end_day - calculated
 
-            money_record.total_day = total_day
-            money_record.total_cash = sum_nal
-            money_record.total_bn = sum_bn
-            money_record.total_market = yandex + delivery
-            money_record.total_expenses = expenses
-            money_record.total_salary = salary_avans + salary_calculated
-            money_record.total_payin = payin
-            money_record.total_payout = payout
-            money_record.calculated = calculated
-            money_record.difference = money_record.sum_cash_end_day - calculated
-
-            if money_record.session:
-                money_record.session.storage = storage
-                money_record.session.date_at = date_at
-                money_record.session.session_number = session_number
-                money_record.session.open_date = open_date
-                money_record.session.close_date = close_date
-                money_record.session.cash = cash
-                money_record.session.cash_point = cash_point
-                money_record.session.yandex = yandex
-                money_record.session.delivery = delivery
-            else:
-                session = Session.objects.create(storage_id=storage.id, date_at=date_at,
-                                                 session_number=session_number, open_date=open_date,
-                                                 close_date=close_date, cash=cash, cash_point=cash_point,
-                                                 yandex=yandex, delivery=delivery)
-                money_record.session_id = session.id
-            money_record.save()
+                if money_record.session:
+                    money_record.session.storage = storage
+                    money_record.session.date_at = date_at
+                    money_record.session.session_number = session_number
+                    money_record.session.open_date = open_date
+                    money_record.session.close_date = close_date
+                    money_record.session.cash = cash
+                    money_record.session.cash_point = cash_point
+                    money_record.session.yandex = yandex
+                    money_record.session.delivery = delivery
+                else:
+                    session = Session.objects.create(storage_id=storage.id, date_at=date_at,
+                                                     session_number=session_number, open_date=open_date,
+                                                     close_date=close_date, cash=cash, cash_point=cash_point,
+                                                     yandex=yandex, delivery=delivery)
+                    money_record.session_id = session.id
+                money_record.save()
         else:
             raise self.model.DoesNotExist('Запись с указанным идентификатором не найдена.')
 
