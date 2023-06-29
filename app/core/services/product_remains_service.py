67522fb5351646fb4110f9d9f700e136344a8d2b
mikhail-luginin <125300555+mikhail-luginin@users.noreply.github.com>
2023-06-29 23:47:08 +0300

    Fix product remains

--- app/core/services/product_remains_service.py
+++ app/core/services/product_remains_service.py
@@ -1,9 +1,18 @@
 from apps.lk.models import ProductRemain
 
 
+def remain_get(storage_id: int, product_id: int) -> ProductRemain | None:
+    return ProductRemain.objects.filter(storage_id=storage_id, product_id=product_id).first()
+
+
 def add_remain(remains: dict, storage_id: int):
     for product_id, amount in remains.items():
-        ProductRemain.objects.create(storage_id=storage_id, product_id=product_id, amount=amount)
+        remain_record = remain_get(storage_id=storage_id, product_id=product_id)
+        if remain_record is None:
+            ProductRemain.objects.create(storage_id=storage_id, product_id=product_id, amount=amount)
+        else:
+            remain_record.amount = amount
+            remain_record.save()
 
 
 def remains_by_storage_id(storage_id: int):
