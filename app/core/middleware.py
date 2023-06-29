ea35c56b731e580875d50c0d6509aa690bcad63c
mikhail-luginin <125300555+mikhail-luginin@users.noreply.github.com>
2023-06-30 00:06:11 +0300

    Add traceback to error message

--- app/core/middleware.py
+++ app/core/middleware.py
@@ -15,6 +15,9 @@ class Process500:
         return self._get_response(request)
 
     def process_exception(self, request, exception):
-        send_message_to_telegram(settings.TELEGRAM_CHAT_ID_FOR_ERRORS, f'Process500 [<b>{request.path}</b>]\n\n{exception}')
+        import traceback
+        trace = traceback.format_exc()
+        send_message_to_telegram(settings.TELEGRAM_CHAT_ID_FOR_ERRORS, f'Process500 [<b>{request.path}</b>]\n\n'
+                                                                       f'{exception}\n\n Traceback: {trace}')
         messages.error(request, 'Произошла неизвестная ошибка.')
         return redirect(request.META.get('HTTP_REFERER', '/'))
